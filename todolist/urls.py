56f665caff57c887a48d4ebbd09128905d31660e
Enzo Crespillo <ecrespillo@eventbrite.com>
2021-04-08 14:04:23 -0300

    lots of changes

--- todolist/urls.py
+++ todolist/urls.py
@@ -21,7 +21,7 @@ from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todolist_app.urls')),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('register/', user_views.register, name='register'),
 ]
